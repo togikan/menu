1c07cc120b6404575dd82c1f8b62a6174091e0ea
Togay Han Kaya <tkaya@swensonhe.com>
2021-03-06 23:30:38 +0300

    Refactor feature module name to item

---
+++ feature_item/src/main/java/com/thk/feature_item/presentation/MenuPresentationModule.kt
@@ -0,0 +1,8 @@
+package com.thk.feature_item.presentation
+
+import com.thk.feature_item.MODULE_NAME
+import org.kodein.di.Kodein
+
+internal val presentationModule = Kodein.Module("${MODULE_NAME}PresentationModule") {
+
+}
