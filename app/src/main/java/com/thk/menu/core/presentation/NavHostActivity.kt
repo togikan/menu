30dbd69829c46c4a2b14f0458a77ec2c61be1580
Togay Han Kaya <tkaya@swensonhe.com>
2021-03-06 22:17:56 +0300

    Enable view binding

--- app/src/main/java/com/thk/menu/core/presentation/NavHostActivity.kt
+++ app/src/main/java/com/thk/menu/core/presentation/NavHostActivity.kt
@@ -1,13 +1,16 @@
 package com.thk.menu.core.presentation
 
 import android.os.Bundle
-import com.thk.menu.R
+import com.thk.menu.base.delegate.viewBinding
 import com.thk.menu.base.presentation.activity.BaseActivity
+import com.thk.menu.databinding.ActivityNavHostBinding
 
 class NavHostActivity : BaseActivity() {
 
+    private val binding: ActivityNavHostBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nav_host)
+        setContentView(binding.root)
     }
 }
